d6528f5b5b69a8f3486a54029fa2d2518ff32938
kenkao <kenkao_01@126.com>
2019-09-26 18:35:22 +0800

    fix

--- toolkit/gen_lobby/lobby_manager.go
+++ toolkit/gen_lobby/lobby_manager.go
@@ -6,20 +6,14 @@ import (
 )
 
 type LobbyEntity struct {
-	LobbyId   string `bson:"lobbyid"`
-	Nick      string `bson:"nick"`
-	RoomCount int
-	PeerCount int
-	Rooms     []LiteRoomEntity
+	LobbyId string `bson:"lobbyid"`
+	Nick    string `bson:"nick"`
 }
 
 func NewLobbyEntity(lobbyId string) *LobbyEntity {
 	l := new(LobbyEntity)
 	l.LobbyId = lobbyId
 	l.Nick = lobbyId
-	l.RoomCount = 0
-	l.PeerCount = 0
-	l.Rooms = make([]LiteRoomEntity, 0, 10)
 	return l
 }
 
@@ -30,12 +24,37 @@ func (l *LobbyEntity) SetNick(nick string) {
 	gamedb.UpdateLobby(fb, op)
 }
 
-func (l *LobbyEntity) CreateRoom(roomId string) (*LiteRoomEntity, error) {
-	lr := NewLiteRoomEntity(roomId, l.LobbyId)
+func (l *LobbyEntity) RoomCount() int {
+
+}
+
+func (l *LobbyEntity) PeerCount() int {
+
+}
+
+func (l *LobbyEntity) Rooms() []LiteRoomEntity {
+
+}
+
+func (l *LobbyEntity) CreateRoom(roomId string, nodeId string) (*LiteRoomEntity, error) {
+	lr := NewLiteRoomEntity(roomId, l.LobbyId, nodeId)
 	if err := gamedb.CreateRoom(lr); err != nil {
 		return nil, err
 	}
 	return lr, nil
+}
+
+func (l *LobbyEntity) FindRoom(roomId string) (*LiteRoomEntity, error) {
+	lr := NewLiteRoomEntity("", "", "")
+	fb := mongodb.NewFilterBuilder().Equal("roomid", roomId).Serialize()
+	if err := gamedb.FindRoom(fb, lr); err != nil {
+		return nil, err
+	}
+	return lr, nil
+}
+
+func (l *LobbyEntity) RandomRoom() (*LiteRoomEntity, error) {
+
 }
 
 type LobbyManager struct {
@@ -68,15 +87,17 @@ func (l *LobbyManager) FindLobby(lobbyId string) (*LobbyEntity, error) {
 	if err := gamedb.FindLobby(filter, lobby); err != nil {
 		return nil, err
 	}
-	rooms := make([]LiteRoomEntity, 0, 10)
-	if err := gamedb.FindRooms(filter, rooms); err != nil {
-		return nil, err
-	}
-	lobby.Rooms = rooms
-	for _, room := range rooms {
-		lobby.RoomCount = lobby.RoomCount + 1
-		lobby.PeerCount = lobby.PeerCount + room.PeerCount
-	}
+	/*
+		rooms := make([]LiteRoomEntity, 0, 10)
+		if err := gamedb.FindRooms(filter, rooms); err != nil {
+			return nil, err
+		}
+		lobby.Rooms = rooms
+		for _, room := range rooms {
+			lobby.RoomCount = lobby.RoomCount + 1
+			lobby.PeerCount = lobby.PeerCount + room.PeerCount
+		}
+	*/
 	return lobby, nil
 }
 
@@ -88,11 +109,12 @@ type LiteRoomEntity struct {
 	PeerCount int    `bson:"peercount"`
 }
 
-func NewLiteRoomEntity(roomId string, lobbyId string) *LiteRoomEntity {
+func NewLiteRoomEntity(roomId string, lobbyId string, nodeId string) *LiteRoomEntity {
 	lr := new(LiteRoomEntity)
 	lr.RoomId = roomId
 	lr.Nick = roomId
 	lr.LobbyId = lobbyId
+	lr.NodeId = nodeId
 	lr.PeerCount = 0
 	return lr
 }
